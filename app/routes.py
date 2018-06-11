4d512f90d9634db86bd61f17d61a6b29975e6399
Jack Beanland <jmcbeanland@gmail.com>
2018-06-11 18:36:27 +0200

    working. still ugly. some names in stop list not correct.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, current_app, redirect
 import json
 
 from app import app
@@ -7,21 +7,76 @@ from app.utils import get_trains
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    print('this is the main page')
-
+    # print('this is the main page')
+    # if 'user_favourite' in request.cookies:
+    #     favs = request.cookies['user_favourite']
+    #     print('favs:', favs)
+    # else:
+    #     print('no fav cookie')
+    #     # print(request.cookies)
+    # print('\n')
     return render_template('main.html', title='Landing')
 
 
 @app.route('/train_times/', methods=['POST'])
 def train_times():
-    print('in train_times')
-    trains = get_trains(request.form['stop_wanted'])
+    # print('in train_times')
+    trains = get_trains(int(request.form['stop_wanted']))
     return json.dumps(trains)
 
 
 @app.route('/get_stops/')
 def get_stops():
-    print('getting stops')
+    # print('getting stops')
     choices = [{'stop_id': v, 'stop': k} for k, v in app.config['STOPS'].items()]
     choices = sorted(choices, key=lambda k: k['stop'])
     return json.dumps(choices)
+
+
+@app.route('/get_favourites/')
+def get_favourites():
+    favs = request.cookies.get('user_favourite', '')
+    if favs != '':
+        # print(favs)
+        favs = request.cookies['user_favourite']
+        favs = favs.split(',')
+        favs = [{'stop_id': s, 'stop_name': app.config['STOPS_REV'][int(s)]} for s in favs]
+    else:
+        favs = []
+    return json.dumps(favs)
+
+
+@app.route('/set_favourite/', methods=['POST'])
+def set_favourite():
+    new_favourite = request.form['stop_id']
+    stop_name = app.config['STOPS_REV'][int(new_favourite)]
+    response = current_app.make_response(json.dumps({'stop_id': int(new_favourite), 'stop_name': stop_name}))
+
+    favs = request.cookies.get('user_favourite', '')
+    if favs != '':
+        favs = ",".join([favs, new_favourite])
+    else:
+        favs = new_favourite
+    response.set_cookie('user_favourite', value=favs)
+
+    # print(json.dumps({'stop_id': int(new_favourite), 'stop_name': stop_name}))
+    return response
+
+
+@app.route('/remove_favourite/', methods=['POST'])
+def remove_favourite():
+    # print('remove fav')
+    stop_id = request.form['stop_id']
+    response = current_app.make_response(redirect('/'))
+    try:
+        existing = request.cookies['user_favourite'].split(',')
+        existing.remove(stop_id)
+        if len(existing) == 0:
+            existing = ''
+        else:
+            existing = ','.join(existing)
+    except (ValueError, AttributeError):
+        existing = ''
+    # print(existing)
+    response.set_cookie('user_favourite', value=existing)
+    return response
